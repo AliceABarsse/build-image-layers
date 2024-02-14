c7b6323214d61d47eb1df76ada3f9e575cbc304d
Alice Barsse <alice.barsse@gmail.com>
2024-02-14 15:39:07 +0100

    Make an underlayer visible method 1
    Cut out underlayer and overlay this visible section, using clipToBounds and a vertical rectangle shape

--- app/src/main/java/fr/caravellecode/imagecompositingexample/MainActivity.kt
+++ app/src/main/java/fr/caravellecode/imagecompositingexample/MainActivity.kt
@@ -3,27 +3,43 @@ package fr.caravellecode.imagecompositingexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,8 +69,15 @@ fun ComposeImages(title: String, modifier: Modifier = Modifier) {
 
     var sizeInPx by remember { mutableStateOf(IntSize.Zero) }
 
-    Column(modifier = Modifier.background(color = Color(0xFFDFDFDF))
-        ) {
+    val configuration = LocalConfiguration.current
+
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+
+    Column(
+        modifier = Modifier.background(color = Color(0xFFDFDFDF))
+    ) {
+
         Text(
             text = "$title!",
             modifier = modifier
@@ -64,37 +87,63 @@ fun ComposeImages(title: String, modifier: Modifier = Modifier) {
         val opaqueDetailsColor = Color.White
         val collarShapeVector = R.drawable.baseline_join_full_24
         val buttonsOutlineVector = R.drawable.outline_buttons_24
-        Box (modifier = Modifier
+
+        val collarSize = 80.dp
+        val centerXOffset = sizeInPx.width.div(2).pxToDp().minus(collarSize / 2)
+
+
+        Box(modifier = Modifier
             .onGloballyPositioned {
                 sizeInPx = it.size
-            }) {
+            }
+        ) {
             Image(
                 modifier = Modifier.offset(x = 0.dp, y = 25.dp),
                 painter = painterResource(id = baseImageBitmap),
                 contentDescription = "Base raster graphic image",
-                colorFilter = ColorFilter.tint(color = opaqueColor.copy(alpha = 0.3f), blendMode = BlendMode.SrcAtop),
+                colorFilter = ColorFilter.tint(color = opaqueColor, blendMode = BlendMode.SrcAtop),
             )
-            val collarSize = 80.dp
-            val centerXOffset = sizeInPx.width.div(2).pxToDp().minus(collarSize/2)
-
             // lay atop more volume elements
             Image(
-                modifier = Modifier.offset(x = centerXOffset, y = 0.dp)
-                    .size(collarSize)
-                ,
+                modifier = Modifier
+                    .offset(x = centerXOffset, y = 0.dp)
+                    .size(collarSize),
                 painter = painterResource(id = collarShapeVector),
                 contentDescription = "collar",
                 colorFilter = ColorFilter.tint(color = opaqueColor),
             )
             // lay atop some outline elements
             Image(
-                modifier = Modifier.offset(x = centerXOffset, y = 55.dp)
-                    .size(collarSize)
-                ,
+                modifier = Modifier
+                    .offset(x = centerXOffset, y = 55.dp)
+                    .size(collarSize),
                 painter = painterResource(id = buttonsOutlineVector),
                 contentDescription = "buttons",
                 colorFilter = ColorFilter.tint(color = opaqueDetailsColor),
             )
+
+            // Make an under-layer "visible" inside a cut-out zone
+            Box (modifier = Modifier.offset(x = 0.dp, y = (-30).dp)
+                .clip(shape = GenericShape{ size, _ ->
+                    // cut out a rectangle in which the under-layer image will appear
+                    moveTo(size.width * 0.65f, 0f)
+                    lineTo(size.width * 0.55f, size.height*0.6f)
+                    lineTo(size.width * 0.60f, size.height)
+                    lineTo(size.width * 0.35f, size.height)
+                    lineTo(size.width * 0.45f, size.height*0.6f)
+                    lineTo(size.width * 0.40f, 0f)
+                })
+                .clipToBounds()
+            ){
+                // black underlayer
+                Image(
+                    modifier = Modifier
+                        .size(400.dp),
+                    painter = painterResource(id = R.mipmap.ic_underlayer_foreground),
+                    contentDescription = "Base raster graphic image",
+                    colorFilter = ColorFilter.tint(color = Color.Black)
+                )
+            }
         }
     }
 }
